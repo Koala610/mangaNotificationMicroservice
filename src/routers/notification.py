56e1a3cbc2a43bc511f4688f4112d5f9feb3c05d
Koala610 <vladli2000610@gmail.com>
2023-05-19 00:16:35 +0600

    feat: changed routes

--- src/routers/notification.py
+++ src/routers/notification.py
@@ -2,12 +2,13 @@ import fastapi
 import config
 import os
 from src.logger import logger
+from typing import Optional
 
 router = fastapi.APIRouter()
 
 def change_env_variable(variable_name: str, new_value: str):
     dot_env_path = ".env"
-    with open(dot_env_path, "r") as file:
+    with open(dot_env_path, "r", encoding="utf-8") as file:
         file_contents = file.readlines()
 
         for i, line in enumerate(file_contents):
@@ -18,13 +19,15 @@ def change_env_variable(variable_name: str, new_value: str):
         file.writelines(file_contents)
 
 @router.get("/notification/service/working_state")
-async def turn_on_off_notifications():
-    config.IS_NOTIFICATION_WORKING = not config.IS_NOTIFICATION_WORKING
-    change_env_variable("IS_NOTIFICATION_WORKING", config.IS_NOTIFICATION_WORKING)
-    return {"message": f"Notification {'not' if not config.IS_NOTIFICATION_WORKING else ''} working"}
+async def turn_on_off_notifications(change: Optional[bool] = True):
+    if change:
+        config.IS_NOTIFICATION_WORKING = not config.IS_NOTIFICATION_WORKING
+        change_env_variable("IS_NOTIFICATION_WORKING", config.IS_NOTIFICATION_WORKING)
+    return config.IS_NOTIFICATION_WORKING
 
 @router.get("/notification/service")
-async def change_update_frequency(update_frequency: int):
-    config.UPDATE_FREQUENCY = update_frequency
-    change_env_variable("UPDATE_FREQUENCY", config.UPDATE_FREQUENCY)
-    return {"message": f"Update frequency now: {config.UPDATE_FREQUENCY}"}
+async def change_update_frequency(update_frequency: Optional[int] = None, change: Optional[bool] = True):
+    if change and update_frequency is not None:
+        config.UPDATE_FREQUENCY = update_frequency
+        change_env_variable("UPDATE_FREQUENCY", config.UPDATE_FREQUENCY)
+    return config.UPDATE_FREQUENCY
