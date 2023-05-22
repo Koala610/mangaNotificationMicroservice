8fbdbdf4e91d64ade3293a00f9cb9680dc513b54
vladlen-li-idf <vladlen.li@solva.kz>
2023-05-22 14:56:46 +0600

    fix: change frequency function, change verifying token

--- src/services/scheduler/main.py
+++ src/services/scheduler/main.py
@@ -4,17 +4,21 @@ import config
 from ..notification import notification_service
 from src.logger import logger
 
-def set_frequency(frequency: int):
+def get_current_update_frequency():
+    return config.UPDATE_FREQUENCY
+
+def set_frequency(get_freq_func):
     logger.info("In decorator")
     def decorator(func):
         async def wrapper(*args, **kwargs):
             while True:
+                frequency = get_freq_func()
                 await asyncio.sleep(frequency)
                 await func(*args, **kwargs)
         return wrapper
     return decorator
 
-@set_frequency(config.UPDATE_FREQUENCY)
+@set_frequency(get_current_update_frequency)
 async def notification_task():
     if config.IS_NOTIFICATION_WORKING:
         logger.info("Sending updates...")
